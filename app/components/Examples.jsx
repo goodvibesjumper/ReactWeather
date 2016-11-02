c2dab5bf9f73219f1e706c77181707449accb8cc
goodvibesjumper <goodvibesjumper@hotmail.com>
2016-11-01 23:03:00 -0600

    Update copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var Examples = (props) => { // new stateless functional component
   return (
-    <h3>Examples Component</h3>
+    <h3>Examples Component!</h3>
   )
 };
 
