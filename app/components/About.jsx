7b30bc760bd3f563c2904d139a39f85f388ae6e1
goodvibesjumper <goodvibesjumper@hotmail.com>
2016-11-02 11:35:27 -0600

    more about page changes

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,11 +4,17 @@ var About = (props) => { // new stateless functional component
   return (
     <div>
       <h1 classname="text-center">About</h1>
-      <p>React Weather App is the bomb-diggity.</p>
-      <p>Who even says that anymore?  bomb diggity</p>
-      <p>Homie please..</p>
-      <p>Here are some links to check out</p>
+      <p>
+        React Weather App is the bomb-diggity.
+        Who even says that anymore?  bomb diggity
+        Homie please..
+        Here are some links to check out
+      </p>
       <ol>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the
+            Javascript framework used.
+        </li>
         <li>
           <a href="https://www.udemy.com/the-complete-react-web-app-developer-course">Andrew Mead's Course on Udemy</a>
         </li>
